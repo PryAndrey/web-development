03f27e25b30b5a2c1258b41b1a4bcd69558e1f20
student <student@school.isphera.ru>
2021-03-01 10:37:28 +0300

    Added IT

---
+++ lw2/02.pas
@@ -0,0 +1,35 @@
+PROGRAM SarahRevere(INPUT, OUTPUT);
+USES DOS;
+VAR
+  Query: STRING;
+  Lanterns: CHAR;
+  I: INTEGER;
+BEGIN {SarahRevere}
+  WRITELN('Content-Type: text/plain');
+  WRITELN;
+  Query := GetEnv('QUERY_STRING');
+  Lanterns := '-';
+
+  IF (Length(Query) > 0)
+  THEN
+    BEGIN
+      I := POS('=', Query) + 1;
+      IF I > 1
+      THEN
+        Lanterns := Query[I];
+    END;
+
+  IF (Lanterns > '0') AND (Lanterns < '3')
+  THEN
+    WRITE('The British are coming by ');
+    IF Lanterns = '1'
+    THEN
+      WRITELN('land.')
+    ELSE
+      IF Lanterns = '2'
+      THEN
+        WRITELN('sea.')
+  ELSE
+    WRITELN('The North Church shows only''', Lanterns, '''.');
+END. {SarahRevere}
+
