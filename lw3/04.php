6556257a11bb4cc5d3f4f3dd21ef5e4eb6f7757d
PryAndrey <79829059+PryAndrey@users.noreply.github.com>
2021-03-14 22:44:17 +0300

    Update 04.php

--- lw3/04.php
+++ lw3/04.php
@@ -1,16 +1,16 @@
 <?php 
 header("Content-Type: text/plain");
 define("DATA_PATH", "data/email.txt");
-$fields_to_bind = [
+$fields = [
     'first_name',
     'last_name',
     'email',
     'age' ];    
-$required_fields = ['email'];
+$important_fields = ['email'];
 $dataset = [];
-foreach ($required_fields as $val)
-    if(empty($_GET[$val])) exit();
-foreach ($fields_to_bind as $field)
+foreach ($important_fields as $value)
+    if(empty($_GET[$value])) exit();
+foreach ($fields as $field)
     if(isset($_GET[$field])) $dataset[$field]=$_GET[$field];
 file_put_contents(DATA_PATH, json_encode($dataset));
 echo('Thanks');
